8a6bc15b1d664905d7ec6bd24fc59db1b54418ea
vnoves <vnoves@e-verse.com>
2023-08-29 11:32:47 +0200

    Creation of project base

---
+++ Layer/Layer/Projects/GetAllProjects.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace Projects
+{
+    public class GetAllProjects
+    {
+        // Empty constructor to avoid import in Dynamo
+        private GetAllProjects() { }
+
+        public static List<Project> GetProjects(string bearerToken)
+        {
+            var client = new RestClient("https://api.layer.team/projects");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+
+            // Headers
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Authorization", $"Bearer {bearerToken}");
+
+            try
+            {
+                IRestResponse response = client.Execute(request);
+
+                if (response.IsSuccessful)
+                {
+                    var projectResponse = JsonConvert.DeserializeObject<ProjectResponse>(response.Content);
+                    return projectResponse.projects;
+                }
+                else
+                {
+                    // Handle non-successful response
+                    throw new Exception($"API call failed with status: {response.StatusCode}, content: {response.Content}");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
+    }
+
+    public class ProjectResponse
+    {
+        public List<Project> projects { get; set; }
+    }
+
+    public class Project
+    {
+        public DateTime createdAt { get; set; }
+        public string createdBy { get; set; }
+        public string company { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public string id { get; set; }
+        public string status { get; set; }
+        public string type { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Dynamo.Layer.Package
+# DynaForge
+
+<h3 align="left">An <a href="https://forge.autodesk.com/" target="_blank">Autodesk Forge</a> extension for <a href="http://dynamobim.org/" target="_blank">Dynamo</a>.</h3>
+<br/>
+
+[<img src="https://github.com/EverseDevelopment/DynaForge/blob/main/Assets/DynamoForge.png" width="443" height="166">](https://github.com/EverseDevelopment/DynaForge/wiki)
+<br/>
+
+If you appreciate the work put into this free package, please vote in support of it on Dynamo's package manager. 
+
+## Getting Started
+You can find details on how to get started [Here](https://github.com/EverseDevelopment/DynaForge/wiki/Getting-Started).
+
+## Current Version
+DynaForge is currently built against the latest Dynamo stable build. At this time that is 2.5.0.
+
+## Contributors
+This package is primarily managed by [E-verse](https://www.e-verse.co/) and by [People Like You™](https://github.com/EverseDevelopment/DynaForge/pulse).
+
+## Help improve DynaForge
+If you're interested in contributing to DynaFroge, just submit a [pull request](https://github.com/EverseDevelopment/DynaForge/pulls) or a [feature request](https://github.com/EverseDevelopment/DynaForge/issues) .
+
+## Installation
+DynaForge is available through Dynamo's built-in package manager.
+
+## About us ##
+
+We are an international mix of AEC professionals, product designers, and software developers. We work together to transform construction requirements into accurate and partnership-driven technological solutions.
+
+<p align="center" width="100%">
+    <a href="https://www.e-verse.com/">
+    <img src="https://github.com/EverseDevelopment/DynaForge/blob/main/Assets/e-verse_logo_no%20slogan.jpg" width="732" height="271" align="center">
+    </a>
+</p>
