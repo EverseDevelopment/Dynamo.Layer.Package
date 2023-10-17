7f598c6abfef1d3df70f8dd93cb93d90e8777d84
Francisco Maranchello <49538907+franmaranchello@users.noreply.github.com>
2023-10-17 12:35:28 -0300

    Updated assembly and solution name, reset resources file.

--- LayerApp/LayerApp/Properties/AssemblyInfo.cs
+++ LayerApp/LayerApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Layer")]
+[assembly: AssemblyTitle("LayerApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Layer")]
+[assembly: AssemblyProduct("LayerApp")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
